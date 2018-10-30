4329d48dab7c843c590fb4febf4617356a2cc0ce
benrogboe <43584843+benrogboe@users.noreply.github.com>
2018-10-30 19:37:54 -0400

    Add files via upload

---
+++ finalsite/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from finalsite.models import Tag,Category
+
+admin.site.register(Tag)
+'''admin.site.register(Post)'''
+admin.site.register(Category)
+'''admin.site.register(PostTag)'''
+
+
+"PostTag, Post"
